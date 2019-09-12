f90cc0cbe1ee8e3d783096210a1fafee1f05124b
Jeremy Matt <jmatt@uvm.edu>
2019-09-12 10:09:53 -0400

    code working, needs comments and beautification

--- HW01_driver.py
+++ HW01_driver.py
@@ -5,9 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-from geostats_functions import cov
-from geostats_functions import rho
-from geostats_functions import extract_pairs
+from geostats_functions import *
 
 
 #load data
@@ -18,7 +16,7 @@ res = 'Water Resisitivity ohm-m'
 perm = 'Permeability (mD)'
 data= pd.read_excel(filename)
 
-#data = data[:5]
+#data = data[:10]
 
 #Problem 1
 plt.plot(data[res],data[perm],'.')
@@ -31,3 +29,56 @@ covariance = cov(data,variables)
 pearson_rho = rho(data,variables)
 
 pairs = extract_pairs(data,x,y,variables)
+
+
+lags = [3,9,24,36,54,72]
+thetas = [0,90]
+
+stats = {}
+stats[0] = {}
+stats[0,'rho'] = []
+stats[0,'COV'] = []
+stats[0,'SV'] = []
+stats[90] = {}
+stats[90,'rho'] = []
+stats[90,'COV'] = []
+stats[90,'SV'] = []
+
+for lag in lags:
+    for theta in thetas:
+        lag_pairs = extract_lags(pairs,lag,theta,res)
+        if len(lag_pairs)==0:
+            p_rho = np.nan
+            COV = np.nan
+            SV = np.nan
+        else:
+            #
+            #
+            #ADD N VALUES
+            #ALSO DISCUSS THE REASON WHY MORE IN X than Y
+            #SAID IN CLASS THAT THE SLAB WAS INTENTIONALLY SET ON THE
+            #MACHINE THA WAY
+            #
+            #
+            plt.figure()
+            plt.plot(lag_pairs['head'],lag_pairs['tail'],'.')
+            plt.title('theta:{}, lag:{}mm'.format(theta,lag))
+            max_val = max([max(lag_pairs['head']),max(lag_pairs['tail'])])
+            min_val = min([min(lag_pairs['head']),min(lag_pairs['tail'])])
+            plt.plot([min_val,max_val],[min_val,max_val],'r--')
+            p_rho = rho(lag_pairs,['head','tail'])
+            COV = cov(lag_pairs,['head','tail'])
+            SVs = SV(lag_pairs,['head','tail'])
+            
+        stats[theta,'rho'].append(p_rho)
+        stats[theta,'COV'].append(COV)
+        stats[theta,'SV'].append(SVs)
+        
+stat_names = ['rho','COV','SV']
+for stat in stat_names:
+    for theta in thetas:
+        plt.figure()
+        plt.plot(lags,stats[theta,stat],'*--')
+        plt.title('theta:{}'.format(theta))
+        plt.xlabel('lag')
+        plt.ylabel(stat)
--- geostats_functions.py
+++ geostats_functions.py
@@ -15,10 +15,12 @@ def cov(data,variables):
         covariance
         
     """
-    
+    #Calculate the means of each of the variables
     means = data[variables].mean()
-
+    
+    #Calculate the product of the differences from the means
     cov = (data[variables[0]]-means[variables[0]])*(data[variables[1]]-means[variables[1]])
+    #Normalize by N
     cov = cov.mean()
     
     return cov
@@ -27,9 +29,13 @@ def std(data,variable):
     """
     calculates the standard deviation 
     """
+    #Find the mean of the variable
     mean = data[variable].mean()
+    #Square the differences from the mean
     std = (data[variable]-mean)**2
+    #take the square root
     std = std.mean()**.5
+    
     return std
 
 def rho(data,variables):
@@ -43,37 +49,95 @@ def rho(data,variables):
     OUTPUT
         rho - Pearson's rho
     """
-    
+    #Calculate the covariance
     covariance = cov(data,variables)
+    #Calculate the standard deviation of each variable
     std1 = std(data,variables[0])
     std2 = std(data,variables[1])
     
+    #Normalize the covariance by the standard deviations
     rho = covariance/(std1*std2)
     return rho
     
 def extract_pairs(data,x,y,variables):
     """
-    Returns a subset based on a lag and angle
+    Returns all pairs of points (forward and reverse) and the heads and tails 
+    for each variable in the variables list
     """
+    #Initialize an empty pandas dataframe
     deltas = pd.DataFrame()
+    #For each point except the last, check all pairs
+    #NOTE: This approach has a memory complexity of O(2*(N-1)!)
     for i in range(len(data)-1):
-        print('collecting deltas, i={}'.format(i))
+        #Init two empty temporary dataframes
         temp = pd.DataFrame()
+        temp2 = pd.DataFrame()
+        #Extract the current x and y position
         cur_x = data.loc[i,x]
         cur_y = data.loc[i,y]
+        #Find the delta X and delta Y for each pair from the
+        #current point to all other points
         temp['dx'] = data.loc[i+1:,x]-cur_x
         temp['dy'] = data.loc[i+1:,y]-cur_y
+        #Find the delta X and delta Y for each pair from
+        #all other points to the current point
+        temp2['dx'] = cur_x-data.loc[i+1:,x]
+        temp2['dy'] = cur_y-data.loc[i+1:,y]
         for var in variables:
+            #Grab the head and tail for all vars for each pair from the
+            #current point to all other points
             temp['head|'+var] = data.loc[i,var]
-            temp['tail|'+var] = data.loc[i+1,var]
+            temp['tail|'+var] = data.loc[i+1:,var]
+            #Grab the head and tail for all vars for each pair from
+            #all other points to the current point
+            temp2['head|'+var] = data.loc[i+1:,var]
+            temp2['tail|'+var] = data.loc[i,var]
         
+        #Append the temp pairs to the deltas dataframe
         deltas = deltas.append(temp)
+        deltas = deltas.append(temp2)
     
+    #Reset the index
     deltas.reset_index(drop=True,inplace=True)
+    #Insert a column for distance and calculate
     deltas.insert(2,'dist',value=np.nan)
+    #calculate distance
     deltas['dist'] = (deltas['dx']**2+deltas['dy']**2)**.5
+    #Insert a column for angle
     deltas.insert(2,'theta',value=np.nan)
+    #Calculate theta
     deltas['theta'] = np.arctan2(deltas['dy'],deltas['dx'])*180/np.pi
     
     return deltas
+
+def extract_lags(pairs,lag,theta,variable):
+    """
+    Extracts pairs that match the lag and angle and returns only the head 
+    and tail for the specified variable
+    """
+    
+    #create a mask of pairs where the distance equals the lag
+    m1 = pairs['dist']==lag
+    #create a mask of pairs where the angle equals theta
+    m2 = pairs['theta']==theta
+    
+    #Extract the data where both masks are true
+    lag_pairs_all = pairs[m1&m2]
+    #Extract the dx, dy, distance, and theta columns
+    lag_pairs = pd.DataFrame(lag_pairs_all[['dx','dy','dist','theta']])
+    #Extract the variable values for the head and tail
+    lag_pairs['head'] = lag_pairs_all['head|'+variable]
+    lag_pairs['tail'] = lag_pairs_all['tail|'+variable]
+    
+    return lag_pairs
   
+def SV(data,variables):
+    """
+    Calculates and returns the semivariance between two variables
+    """
+    #Find the size of the 
+    N = data.shape[0]
+    squares = (data[variables[0]]-data[variables[1]])**2
+    sum_squares = squares.sum()
+    SV = sum_squares/(2*N)
+    return SV
